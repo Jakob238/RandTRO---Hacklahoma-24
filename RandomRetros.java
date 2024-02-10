aa7266bcd2dd9a07b58d9b26b39dda676637f0e6
Calvin Nguyen <calvintnguyen2004@gmail.com>
2024-02-10 15:44:58 -0600

    changed to main method

--- RandomRetros.java
+++ RandomRetros.java
@@ -6,7 +6,15 @@ public class RandomRetros extends JPanel
 {
     // Instances Variables
     private static final String WINDOW_TITLE = "Random Retro Project!";
-    JPanel panel = new RandomRetros();
-    JFrame frame = new JFrame(WINDOW_TITLE);
+
+    public static void main(String[] args) {
+
+        JPanel panel = new RandomRetros();
+        JFrame frame = new JFrame(WINDOW_TITLE);
+        
+    }
+
+    
+
 
 }
