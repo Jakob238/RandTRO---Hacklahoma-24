963c75f485c5f7cd173448fca1074b4d62fd4b17
Calvin Nguyen <calvintnguyen2004@gmail.com>
2024-02-10 15:37:56 -0600

    amongus

--- RandomRetros.java
+++ RandomRetros.java
@@ -5,5 +5,11 @@ import java.awt.geom.*;
 public class RandomRetros extends JPanel
 {
     // Instances Variables
+    private static final String WINDOW_TITLE = "Random Retro Project!";
+    JPanel panel = new RandomRetros();
+    JFrame frame = new JFrame(WINDOW_TITLE);
+
     
+
+
 }
