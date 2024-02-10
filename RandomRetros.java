c7db7e10a7744c04320677b4bfac18c08a9c9199
Jakob238 <linenberger.jakob456@gmail.com>
2024-02-10 16:09:09 -0600

    added spacebar method

--- RandomRetros.java
+++ RandomRetros.java
@@ -1,20 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.event.*;
 
 public class RandomRetros extends JPanel
 {
     // Instances Variables
     private static final String WINDOW_TITLE = "Random Retro Project!";
-
-    public static void main(String[] args) {
-
+public static void main(String[] args)
+    {
         JPanel panel = new RandomRetros();
         JFrame frame = new JFrame(WINDOW_TITLE);
-        
+   
+
+
+
+
+
+    panel.setFocusable(true); 
+    panel.requestFocus(); 
+    panel.addKeyListener(new KeyAdapter()
+        {
+    public void pressKey(KeyEvent e)
+    {
+        if(e.getKeyCode()== KeyEvent.VK_SPACE)
+        {
+            //call get method for random image
+        }
     }
-
-    
-
-
+        });
+    }
 }
+
