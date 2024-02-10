f5e487cc91c1d0bfbc7e057c0e4b2631d5f328f0
Calvin Nguyen <calvintnguyen2004@gmail.com>
2024-02-10 16:18:06 -0600

    hi

--- RandomRetros.java
+++ RandomRetros.java
@@ -1,21 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-import java.awt.event.*;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class RandomRetros extends JPanel
 {
     // Instances Variables
     private static final String WINDOW_TITLE = "Random Retro Project!";
-public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
+
         JPanel panel = new RandomRetros();
         JFrame frame = new JFrame(WINDOW_TITLE);
-        
+        JLabel label = new JLabel("Test Label");
+
+        frame.setSize(500, 500);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(label);
+        frame.setVisible(true);
+
+
+
+
+
+
+
     }
 
     
 
 
 }
-
